dbdc826adfea1a14893cf28821aef5ebdb153090
suhyung kwon <ospac111@gmail.com>
2019-01-25 03:25:55 +0900

    first

---
+++ intro/insertionSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX 100000
+int main(void){
+    int list[MAX];
+    int size,i,j,key;
+    scanf("%d",&size);
+    for(i = 0; i < size; i++){
+        scanf("%d",&list[i]);
+    }
+    for(i = 1; i < size; i++){
+        key = list[i];
+        j = i - 1;
+        while(j >= 0 && list[j] > key){
+            list[j + 1] = list[j];
+            j--;
+        }
+        list[j + 1] = key;
+    }
+    for(i = 0; i < size; i++){
+        printf("%d ", list[i]);
+    }
+}
